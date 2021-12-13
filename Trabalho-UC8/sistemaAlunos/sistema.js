2d506acd7bd259c812e492a5ba68680a25755236
RaianP <raian840459@gmail.com>
2021-12-13 14:57:18 -0300

    Ajuste sistemaAluno

--- Trabalho-UC8/sistemaAlunos/sistema.js
+++ Trabalho-UC8/sistemaAlunos/sistema.js
@@ -1,13 +1,34 @@
-let numeroAlunos = 10;
-
-for (let contador = 0; contador <= numeroAlunos; contador++) {
-    //console.log(contador)
-
- if(contador == 0){
-     console.log("O número atual é zero")
- }   else if(contador % 2 == 0){
-     console.log("O Número" + contador + " é Par")
- } else{
-     console.log(`O número ${contador} é Impar`)
- }
+let numeroDeAlunos = 10;
+// exp for
+for (let contador = 0; contador <= numeroDeAlunos; contador++) {
+    // console.log(contador)
+    
+if(contador == 0){
+    console.log("O número atual é zero")
+}else if(contador % 2 == 0){
+    console.log("O número " + contador + " é Par")
+}else{ 
+    console.log(`O numero ${contador} é Impar`)
 }
+}
+
+// exp while
+let contador = 0;
+
+while(contador <= numeroDeAlunos) {
+    
+if(contador == 0){
+    console.log("O número atual é zero")
+}else if(contador % 2 == 0){
+    console.log("O número " + contador + " é Par")
+}else{ 
+    console.log(`O numero ${contador} é Impar`)
+}
+contador++;
+}
+
+// exp for of
+let nomeDeAluno = ["Maria", "Francisco", "Ronaldo", "Ana"]
+
+for (let nome of nomeDeAluno)
+console.log(`Esta pessoa se chama ${nome}`)
